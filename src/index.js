0b4ce31a80a91e73fa6eae82a5a61973ebc446d9
PashaShilik <pasha-shilik@mail.ru>
2023-01-06 14:07:48 +0300

    feat: create test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+  const numZeroNineteen = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  const numDozens = ['zero','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  const numInMas = String(number).split('');
+
+  let outStr;
+  if (numInMas.length === 3 && number % 100 === 0){
+   outStr = `${numZeroNineteen[numInMas[0]]} hundred`;
+  } else if (numInMas.length === 2 && number % 10 === 0){
+    outStr = `${numDozens[numInMas[0]]}`;
+  } else if (number <= 19){
+   outStr = `${numZeroNineteen[number]}`;
+  } else if (numInMas.length === 3 && number % 100 <= 19){
+    outStr = `${numZeroNineteen[numInMas[0]]} hundred ${numZeroNineteen[number % 100]}`; 
+  } else if (numInMas.length === 3 && number % 100 > 19 && number % 10 !== 0){
+    outStr = `${numZeroNineteen[numInMas[0]]} hundred ${numDozens[numInMas[1]]} ${numZeroNineteen[numInMas[2]]}`; 
+  } else if (numInMas.length === 3 && number % 100 > 19 && number % 10 === 0){
+    outStr = `${numZeroNineteen[numInMas[0]]} hundred ${numDozens[numInMas[1]]}`; 
+  } else if (numInMas.length === 2 && number > 19){
+    outStr = `${numDozens[numInMas[0]]} ${numZeroNineteen[numInMas[1]]}`;
+  };
+  return outStr;
 }
+
